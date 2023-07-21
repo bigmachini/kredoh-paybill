c8302220aac49176141a7503c1a01900699cc7dd
bigmachini <elisha.wambiji@bigmachini.net>
2023-07-21 11:51:38 +0300

    add log)name

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from google.cloud import logging
 logging_client = logging.Client()
 
 # The name of the log to write to
-log_name = "kredoh-paybill"
+log_name = "kredoh_paybill_api_onpremise"
 # Selects the log to write to
 _logger = logging_client.logger(log_name)
